515e703736e8f86f5ad82504a296c66db1e99aa7
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-01-24 21:25:24 +0900

    Add: run.sh

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from tqdm import tqdm
 
 import time
--- Crawling/run.sh
+++ Crawling/run.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 
+# install pip library
+pip install lxml
+
 # Set start and end page
 START_PAGE=358
 END_PAGE=389
 SLEEP_TIME=2
-MODE=headless
-USE_PROXY=""
-PROXY_PATH=""
+MODE=server
 
 # USE_PROXY="--use_proxy"
 # PROXY_PATH="--proxy_path 27.107.27.9:80"
 
 
-python3 crawling.py --start_page $START_PAGE --end_page $END_PAGE \
---mode $MODE --num_workers 1 --sleep_time $SLEEP_TIME $USE_PROXY $PROXY_PATH
+python crawling.py --start_page $START_PAGE --end_page $END_PAGE --mode $MODE
