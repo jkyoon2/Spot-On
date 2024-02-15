c432bbd0191400dd73814c27e7cc94d9f769c553
Hoesu Chung <treecollector@yonsei.ac.kr>
2024-02-15 22:30:29 +0900

    UPDATE

--- Modeling/text_search.py
+++ Modeling/text_search.py
@@ -1,25 +1,43 @@
+import os
 import torch
+import pickle
 from typing import List
+from transformers import CLIPModel, CLIPProcessor
 
-class TextSearchModel:
-    def __init__(self):
+class TextSearchModel():
+    def __init__(self, pickle_path: str, image_dir: str, device):
         self.num_images = 1000
+
         # TODO 1: Initialize CLIP text model
+        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
         # TODO 2: Load encoded image features (pickle file)
+        with open(pickle_path, "rb") as file:
+            self.pickle = pickle.load(file)
+
         # TODO 3: Initialize image file path list ('./images/000000.jpg', ...)
+        self.image_file_path_list = [os.path.join(image_dir, image) for image in os.listdir(image_dir)]
+
         # TODO 4: Initialize Papago API client
+        self.papago = None
+
         # TODO 5: Initialize hyperparameter of weights for each similarity score
+        self.weights = None
 
     def encode_text(self, query_text: str) -> torch.Tensor:
         # TODO 1: Encode the query_text using CLIP text model
-        return torch.zeros(512)
+        text_token = self.processor(query_text, return_tensors="pt", padding=True, truncation=True).to(self.device)
+        with torch.no_grad():
+            text_embed = self.model.get_text_features(**text_token)
+        return text_embed
     
     def calculate_text_similarity(self, query_text_emb: torch.Tensor) -> List[float]:
         # TODO 4: Calculate similarity scores between the query_text and,
         # 1. All big category names (only 1 for each image)
         # 2. All small category names (only 1 for each image)
         # 3. All image hashtags (>= 0 hashtags in the image, may vary per image)
-
+        
         # TODO 5: Calculate the overall similarity score for each image
         # 1. query text vs big category names: List[float], length = num_images
         # 2. query text vs small category names: List[float], length = num_images
